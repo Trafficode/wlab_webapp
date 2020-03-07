918fd59250ad55b5f5565c45e653944b2df3b80d
kku <kku@kku-CELSIUS>
2020-03-07 14:26:16 +0100

    1.6

--- globals.py
+++ globals.py
@@ -7,10 +7,10 @@
 
 class Globals(object):
     
-    WLAB_VERSION     = "1.5"
+    WLAB_VERSION     = "1.6"
     WLAB_COMMIT_DATE = '2020-03-07'
     
-    RELEASE_CONFIG_FILE  = '/home/wlab/weatherlab/config/config_wlabwebapp.json'
+    RELEASE_CONFIG_FILE  = '/home/wlab/weatherlab/config/wlabwebapp.json'
     
     EXIT_CODE_REBOOT     = 10
     EXIT_CODE_EXIT       = 11
